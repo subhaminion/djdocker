adaabf951994069553b76b58857294475ff342fc
subham <subham.bhattacharjee1@gmail.com>
2018-10-11 01:35:07 +0530

    final

---
+++ main/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from main.models import CSVData
+
+admin.site.register(CSVData)
--- main/models.py
+++ main/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class CSVData(models.Model):
     key = models.CharField(max_length=100)
     value = models.IntegerField()
+
+    def __str__(self):
+        return self.key
